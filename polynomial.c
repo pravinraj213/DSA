dbc6ccbf7862e88725c037688be6e6ceffc5ad7d
Pravinraj P <192765409+pravinraj213@users.noreply.github.com>
2025-06-04 17:57:26 +0530

    Update polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -1,104 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node to represent a term in the polynomial
+// Node structure for polynomial term
 struct Node {
     int coeff;
     int power;
     struct Node* next;
 };
 
-// Function to create a new term node
-struct Node* createNode(int coeff, int power) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->coeff = coeff;
-    newNode->power = power;
-    newNode->next = NULL;
-    return newNode;
+// Create new node
+struct Node* createNode(int c, int p) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->coeff = c;
+    temp->power = p;
+    temp->next = NULL;
+    return temp;
 }
 
-// Insert term in decreasing order of power
-void insertTerm(struct Node** poly, int coeff, int power) {
-    struct Node* newNode = createNode(coeff, power);
-
-    // First node or insert before head
-    if (*poly == NULL || power > (*poly)->power) {
-        newNode->next = *poly;
+// Insert term at end (assumes input polynomials sorted by power desc)
+void insertTerm(struct Node** poly, int c, int p) {
+    struct Node* newNode = createNode(c, p);
+    if (*poly == NULL) {
         *poly = newNode;
-    }
-    else {
+    } else {
         struct Node* temp = *poly;
-        while (temp->next != NULL && temp->next->power > power)
-            temp = temp->next;
+        while (temp->next != NULL) temp = temp->next;
+        temp->next = newNode;
+    }
+}
 
-        // If power already exists, add coefficients
-        if (temp->next != NULL && temp->next->power == power) {
-            temp->next->coeff += coeff;
-            free(newNode);
+// Add two polynomials
+struct Node* addPoly(struct Node* p1, struct Node* p2) {
+    struct Node* result = NULL;
+    while (p1 != NULL && p2 != NULL) {
+        if (p1->power > p2->power) {
+            insertTerm(&result, p1->coeff, p1->power);
+            p1 = p1->next;
+        } else if (p2->power > p1->power) {
+            insertTerm(&result, p2->coeff, p2->power);
+            p2 = p2->next;
         } else {
-            newNode->next = temp->next;
-            temp->next = newNode;
+            int sum = p1->coeff + p2->coeff;
+            if (sum != 0) insertTerm(&result, sum, p1->power);
+            p1 = p1->next;
+            p2 = p2->next;
         }
     }
+    // Add remaining terms
+    while (p1 != NULL) {
+        insertTerm(&result, p1->coeff, p1->power);
+        p1 = p1->next;
+    }
+    while (p2 != NULL) {
+        insertTerm(&result, p2->coeff, p2->power);
+        p2 = p2->next;
+    }
+    return result;
 }
 
 // Display polynomial
-void display(struct Node* poly) {
-    if (poly == NULL) {
-        printf("0\n");
-        return;
-    }
-
+void displayPoly(struct Node* poly) {
     while (poly != NULL) {
         printf("%dx^%d", poly->coeff, poly->power);
-        if (poly->next != NULL && poly->next->coeff >= 0)
-            printf(" + ");
+        if (poly->next != NULL) printf(" + ");
         poly = poly->next;
     }
     printf("\n");
 }
 
-// Add two polynomials and return the result
-struct Node* addPolynomials(struct Node* poly1, struct Node* poly2) {
-    struct Node* result = NULL;
-
-    while (poly1 != NULL && poly2 != NULL) {
-        if (poly1->power > poly2->power) {
-            insertTerm(&result, poly1->coeff, poly1->power);
-            poly1 = poly1->next;
-        }
-        else if (poly1->power < poly2->power) {
-            insertTerm(&result, poly2->coeff, poly2->power);
-            poly2 = poly2->next;
-        }
-        else {
-            insertTerm(&result, poly1->coeff + poly2->coeff, poly1->power);
-            poly1 = poly1->next;
-            poly2 = poly2->next;
-        }
-    }
-
-    // Add remaining terms
-    while (poly1 != NULL) {
-        insertTerm(&result, poly1->coeff, poly1->power);
-        poly1 = poly1->next;
-    }
-
-    while (poly2 != NULL) {
-        insertTerm(&result, poly2->coeff, poly2->power);
-        poly2 = poly2->next;
-    }
-
-    return result;
-}
-
-// Main function to demonstrate operations
 int main() {
-    struct Node* poly1 = NULL;
-    struct Node* poly2 = NULL;
-    struct Node* sum = NULL;
+    struct Node *poly1 = NULL, *poly2 = NULL, *sum = NULL;
 
-    // Polynomial 1: 3x^4 + 2x^3 + x
+    // Polynomial 1: 3x^4 + 2x^3 + x^1
     insertTerm(&poly1, 3, 4);
     insertTerm(&poly1, 2, 3);
     insertTerm(&poly1, 1, 1);
@@ -109,15 +82,15 @@ int main() {
     insertTerm(&poly2, 1, 0);
 
     printf("Polynomial 1: ");
-    display(poly1);
+    displayPoly(poly1);
 
     printf("Polynomial 2: ");
-    display(poly2);
+    displayPoly(poly2);
 
-    sum = addPolynomials(poly1, poly2);
+    sum = addPoly(poly1, poly2);
 
     printf("Sum: ");
-    display(sum);
+    displayPoly(sum);
 
     return 0;
 }
