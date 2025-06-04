50420a10b39506c63e0d2e9b74d227cdf8ef6066
Pravinraj P <192765409+pravinraj213@users.noreply.github.com>
2025-06-05 00:26:51 +0530

    Create insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {7, 4, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted Array: ");
+    printArray(arr, n);
+
+    return 0;
+}
