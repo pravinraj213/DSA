2f9633d022a702043d1d0b2eae630d60f8b28b34
Pravinraj P <192765409+pravinraj213@users.noreply.github.com>
2025-06-04 19:35:07 +0530

    Update polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure for polynomial term
 struct Node {
     int coeff;
     int power;
     struct Node* next;
 };
 
-// Create a new polynomial term
 struct Node* createNode(int c, int p) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->coeff = c;
@@ -17,46 +15,18 @@ struct Node* createNode(int c, int p) {
     return newNode;
 }
 
-// Insert a term at the end of the polynomial and return updated head
-struct Node* insertTerm(struct Node* head, int c, int p) {
+struct Node* insertTerm(struct Node* poly, int c, int p) {
     struct Node* newNode = createNode(c, p);
-    if (head == NULL)
-        return newNode;
-
-    struct Node* temp = head;
-    while (temp->next != NULL)
-        temp = temp->next;
+    if (!poly) return newNode;
+    struct Node* temp = poly;
+    while (temp->next) temp = temp->next;
     temp->next = newNode;
-    return head;
+    return poly;
 }
 
-// Display the polynomial
-void display(struct Node* poly) {
-    while (poly != NULL) {
-        if (poly->coeff == 0) {
-            poly = poly->next;
-            continue;
-        }
-
-        if (poly->power == 0)
-            printf("%d", poly->coeff);
-        else if (poly->power == 1)
-            printf("%dx", poly->coeff);
-        else
-            printf("%dx^%d", poly->coeff, poly->power);
-
-        if (poly->next != NULL)
-            printf(" + ");
-        poly = poly->next;
-    }
-    printf("\n");
-}
-
-// Add or subtract two polynomials (sign = 1 for addition, -1 for subtraction)
-struct Node* operatePoly(struct Node* p1, struct Node* p2, int sign) {
+struct Node* polyOperation(struct Node* p1, struct Node* p2, int sign) {
     struct Node* result = NULL;
-
-    while (p1 != NULL && p2 != NULL) {
+    while (p1 && p2) {
         if (p1->power > p2->power) {
             result = insertTerm(result, p1->coeff, p1->power);
             p1 = p1->next;
@@ -65,51 +35,54 @@ struct Node* operatePoly(struct Node* p1, struct Node* p2, int sign) {
             p2 = p2->next;
         } else {
             int sum = p1->coeff + sign * p2->coeff;
-            result = insertTerm(result, sum, p1->power);
+            if (sum != 0)
+                result = insertTerm(result, sum, p1->power);
             p1 = p1->next;
             p2 = p2->next;
         }
     }
-
-    while (p1 != NULL) {
+    while (p1) {
         result = insertTerm(result, p1->coeff, p1->power);
         p1 = p1->next;
     }
-
-    while (p2 != NULL) {
+    while (p2) {
         result = insertTerm(result, sign * p2->coeff, p2->power);
         p2 = p2->next;
     }
-
     return result;
 }
 
-// Main function to demonstrate polynomial addition and subtraction
+// Display polynomial without '+' before first positive term
+void display(struct Node* poly) {
+    int first = 1; // flag for first term
+    while (poly) {
+        if (poly->coeff > 0 && !first) printf("+");
+        printf("%dx^%d ", poly->coeff, poly->power);
+        poly = poly->next;
+        first = 0;
+    }
+    printf("\n");
+}
+
 int main() {
-    struct Node* poly1 = NULL;
-    struct Node* poly2 = NULL;
-    struct Node* sum = NULL;
-    struct Node* diff = NULL;
+    struct Node *p1 = NULL, *p2 = NULL, *sum = NULL, *diff = NULL;
 
-    // Polynomial 1: 3x^3 + 2x^2 + 1
-    poly1 = insertTerm(poly1, 3, 3);
-    poly1 = insertTerm(poly1, 2, 2);
-    poly1 = insertTerm(poly1, 1, 0);
+    p1 = insertTerm(p1, 3, 3);
+    p1 = insertTerm(p1, 2, 2);
+    p1 = insertTerm(p1, 1, 0);
 
-    // Polynomial 2: 2x^2 + 4x + 5
-    poly2 = insertTerm(poly2, 2, 2);
-    poly2 = insertTerm(poly2, 4, 1);
-    poly2 = insertTerm(poly2, 5, 0);
+    p2 = insertTerm(p2, 5, 2);
+    p2 = insertTerm(p2, 4, 1);
+    p2 = insertTerm(p2, 2, 0);
 
-    printf("Polynomial 1: ");
-    display(poly1);
+    printf("Poly 1: ");
+    display(p1);
 
-    printf("Polynomial 2: ");
-    display(poly2);
+    printf("Poly 2: ");
+    display(p2);
 
-    // Perform addition and subtraction
-    sum = operatePoly(poly1, poly2, 1);
-    diff = operatePoly(poly1, poly2, -1);
+    sum = polyOperation(p1, p2, 1);
+    diff = polyOperation(p1, p2, -1);
 
     printf("Sum: ");
     display(sum);
@@ -121,10 +94,9 @@ int main() {
 }
 
 /*
-Expected Output:
------------------
-Polynomial 1: 3x^3 + 2x^2 + 1
-Polynomial 2: 2x^2 + 4x + 5
-Sum: 3x^3 + 4x^2 + 4x + 6
-Difference: 3x^3 + 0x^2 + -4x + -4
+Output:
+Poly 1: 3x^3 2x^2 1x^0 
+Poly 2: 5x^2 4x^1 2x^0 
+Sum: 3x^3 7x^2 4x^1 3x^0 
+Difference: 3x^3 -3x^2 -4x^1 -1x^0 
 */
